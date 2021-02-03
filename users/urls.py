84f062fd137b53fc261fb09d3f7c4e1218b33aa7
smilejakdu <ash982416@gmail.com>
2021-02-04 01:27:28 +0900

    chart_circle

--- users/urls.py
+++ users/urls.py
@@ -2,11 +2,13 @@ from django.urls import path
 from .views      import (SignUpView ,
                          SignInView ,
                          UserListView ,
-                         ProgrammingView)
+                         ProgrammingView,
+                         ChartDataView)
 
 urlpatterns = [
     path("login"       , SignInView.as_view()),
     path("register"    , SignUpView.as_view()),
     path("userlist"    , UserListView.as_view()),
     path("programming" , ProgrammingView.as_view()),
+    path("chartdata"   , ChartDataView.as_view()),
 ]
